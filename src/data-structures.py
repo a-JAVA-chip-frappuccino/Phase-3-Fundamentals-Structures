a2e1be7630a40c6050c730d225d5df1835d2ed3f
Eleanor Kelman <ekelman3@gmail.com>
2024-01-30 09:52:55 -0500

    commented finished lecture

--- src/data-structures.py
+++ src/data-structures.py
@@ -1,7 +1,97 @@
-# lists and list comprehension
+'''
+    LISTS
+'''
 
-# dictionaries
+'''
+    lists are mutable
+    lists are ordered (0-indexed) and can contain duplicates
+'''
 
-# sets
+nums = [1, 2, 3, 4, 5]
 
-# tuples
+print(nums[:2]) # non-destructive
+
+nums.append(6) # destructive
+
+nums[5] = "six"
+
+print(nums.pop(2)) # destructive
+
+nums2 = [1, 2, 3, 4, 5, 6]
+
+squares = []
+
+for num in nums2:
+    num_squared = num ** 2
+    squares.append(num_squared)
+
+# list comprehension
+squares2 = [num ** 2 for num in nums2]
+
+even_cubes = []
+
+for num in nums2:
+    if num % 2 == 0:
+        even_cubes.append(num * num * num)
+
+# list comprehension
+even_cubes2 = [num * num * num for num in nums2 if num % 2 == 0]
+
+print(even_cubes2)
+
+'''
+    DICTIONARIES
+'''
+
+'''
+    dictionaries are mutable
+    dictionaries contain key:value pair mapping and cannot contain duplicate keys
+'''
+
+ages = {"Eleanor" : 25, "Tim" : 32, "Jay": 29, "Andrew" : 23, "Ethan" : 31, "Isaiah" : 33, "Dave" : 37, "Kylie" : 28, "Kevin" : 29}
+
+ages["Tyler"] = 30 # adds new key:value pair
+
+ages["Dave"] = 15 # changes existent value
+
+for key in ages:
+    print(ages[key])
+
+'''
+    SETS
+'''
+
+'''
+    sets are mutable
+    sets are unordered and do not contain duplicates
+'''
+
+plants = set()
+
+buildings = {"Empire State Building", "Chrysler Building", "Washington Monument"}
+
+print(buildings)
+
+plants.add("tree")
+plants.add("flower")
+plants.add("grass")
+plants.add("flower")
+plants.add("dandelion")
+
+print(plants)
+
+print(len(plants)) # gets number of items
+
+'''
+    TUPLES
+'''
+
+'''
+    tuples are immutable
+    tuples are ordered (0-indexed) and have a set length
+'''
+
+coordinate = (20, 30)
+color = (3, 200, 0)
+
+print(color)
--- src/fundamentals.py
+++ src/fundamentals.py
@@ -1,11 +1,89 @@
-# variables and data types
-    # string concatenation: "string1" + "string2"
-    # f-string interpolation: f"{string1}{string2}"
+'''
+    VARIABLES AND DATA TYPES
+'''
 
-# conditional statements
+'''
+    string concatenation: "string1" + "string2"
+    f-string interpolation: f"{string1}{string2}"
+'''
 
-# loops and sequences
-    # for loops
-    # while loops
+name = "Lantz" # string
+favorite_color = 'purple'
+age = 25 # integer
 
-# functions
+print("I am " + str(age) + " years old.") # typecasting
+print(f"I am {age} years old.")
+
+'''
+    CONDITIONAL STATEMENTS
+'''
+
+if name == 'Eleanor' or name == "Lantz":
+    print("You are teaching the lecture!")
+
+if favorite_color == 'green':
+    print("Your favorite color is green!")
+else:
+    print("Your favorite color is not green!")
+
+if age < 0:
+    print("Negative ages don't exist!")
+elif age > -1 and age < 31:
+    print("You are a child!")
+else:
+    print("You are an adult!")
+
+'''
+    LOOPS AND SEQUENCES
+'''
+
+for i in range(10):
+    print(i)
+
+for i in range(5, 10):
+    print(i)
+
+for i in range(5, 10, 2):
+    print(i)
+
+for i in range(10, -1, -1):
+    print(i)
+
+for letter in name:
+    print(letter)
+
+counter = 0
+
+while counter < age:
+    print(counter)
+    counter = counter + 1 # counter += 1
+
+# infinite loop: AVOID!
+# while True:
+#     print("help")
+
+'''
+    FUNCTIONS
+'''
+
+def say_hello():
+    print("Hello!")
+
+say_hello()
+
+def say_hello_to_someone(name = "Jay"):
+    print("Hello there, " + name + "!")
+
+say_hello_to_someone("Isaiah")
+
+def say_color_and_food(fav_color, fav_food):
+    print(f"Your favorite food is {fav_food} and your favorite color is {fav_color}!")
+
+say_color_and_food("red", "pancakes")
+
+def get_book():
+    return "The Great Gatsby"
+
+worst_book = get_book()
+
+print(worst_book)
